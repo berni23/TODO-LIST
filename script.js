28b7fa8c191a97c8860758049bf9ba2ddcbf4f49
berni23 <bernatferrerm@gmail.com>
2020-08-24 20:34:44 +0200

    event wrappers created

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ var acc = document.querySelector(".accordion");
 var panel = document.getElementsByClassName("panel");
 
 
+for (let i = 0; i < panel.length; i++) {
+
+    panel[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        /*panel[(i + 1) % panel.length].classList.remove("active");
+        panel[(i + 2) % panel.length].classList.remove("active");
+        */
+    });
+}
 
 acc.addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
@@ -9,8 +18,6 @@ acc.addEventListener("click", function () {
     this.classList.toggle("active");
     document.getElementById("caret-down").classList.toggle("hidden");
     document.getElementById("caret-right").classList.toggle("hidden");
-
-    var panel = document.getElementsByClassName("panel");
 
     for (let i = 0; i < panel.length; i++) {
         panel[i].classList.toggle("hidden");
