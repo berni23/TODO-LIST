b3bfc44feb0c196df819b02f2d06c0237281a9c5
berni23 <bernatferrerm@gmail.com>
2020-08-24 15:45:40 +0200

    header and sidebar

---
+++ script.js
@@ -0,0 +1,30 @@
+var acc = document.querySelector(".accordion");
+
+var panel = document.getElementsByClassName("panel");
+
+
+
+
+acc.addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+    document.getElementById("caret-down").classList.toggle("hidden");
+    document.getElementById("caret-right").classList.toggle("hidden");
+
+    var panel = document.getElementsByClassName("panel");
+
+    for (let i = 0; i < panel.length; i++) {
+
+        panel[i].classList.toggle("hidden");
+    }
+
+    /* Toggle between hiding and showing the active panel 
+
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+
+    } else {
+        panel.style.display = "block";
+    }*/
+});
