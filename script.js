7ea2991c14eb4428819aac43ec6457ceb9a9d9d5
berni23 <bernatferrerm@gmail.com>
2020-08-25 11:41:27 +0200

    sidebar options completed

--- script.js
+++ script.js
@@ -8,35 +8,26 @@ var taskTitle = document.querySelector(".task-title");
 var filterTodo = document.getElementById("filter-todo");
 var filterCompleted = document.getElementById("filter-completed");
 
-
 for (let i = 0; i < panelClick.length; i++) {
-
     panelClick[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        if (this == filterTodo) {
-            filterCompleted.classList.remove("active");
 
-        } else if (this == filterCompleted) {
-            filterTodo.classList.remove("active");
+
+
+        for (let j = 0; j < panelClick.length; j++) {
+
+
+            panelClick[j].classList.remove("active");
+
         }
-        /*panel[(i + 1) % panel.length].classList.remove("active");
-        panel[(i + 2) % panel.length].classList.remove("active");
-        */
+        this.classList.toggle("active");
+
+
+
+        panelClick[(i + 1) % panelClick.length].classList.remove("active");
+        panelClick[(i + 2) % panelClick.length].classList.remove("active");
+
     });
 }
-
-
-acc.addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    document.getElementById("caret-down-acc").classList.toggle("hidden");
-    document.getElementById("caret-right-acc").classList.toggle("hidden");
-
-    for (let i = 0; i < panelTask.length; i++) {
-        panelTask[i].classList.toggle("hidden");
-    }
-
-});
 
 
 customListSelector.addEventListener("click", function () {
